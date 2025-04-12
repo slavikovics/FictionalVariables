a9904b7de16b8cf15c205bd93837d1f826a31de3
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-04-12 09:28:41 +0300

    Adding language translation

--- FictionalVariablesConsole/Program.cs
+++ FictionalVariablesConsole/Program.cs
@@ -8,9 +8,10 @@ class Program
     {
         while (true)
         {
-            Console.WriteLine("Input logical formula");
+            Console.WriteLine("Введите формулу сокращённого языка логики высказываний. Или введите 'exit', чтобы завершить выполнение программы.");
             var formula = Console.ReadLine();
             if (formula is null) continue;
+            if (formula == "exit") return;
             
             Console.WriteLine();
             PrintFictionalVariables(formula);
@@ -22,25 +23,44 @@ class Program
     {
         try
         {
-            if (!FormulaStringChecker.Check(formula)) throw new Exception("Not logical formula");
+            formula = TranslateFormulaToInnerLanguage(formula);
+            
+            if (!FormulaStringChecker.Check(formula)) 
+                throw new Exception("Введённое выражение не является формулой сокращённого языка логики высказываний.");
+            
             var fictionalVariablesFinder = new FictionalVariablesFinder(formula);
             fictionalVariablesFinder.FindFictionalVariables();
             
-            Console.WriteLine("Found fictional variables:");
+            Console.WriteLine("Найденные фиктивные переменные:");
             foreach (var variable in fictionalVariablesFinder.FictionalVariables)
             {
-                Console.WriteLine(variable);
+                Console.WriteLine(variable.ToUpper());
             }
 
             if (fictionalVariablesFinder.FictionalVariables.Count == 0)
             {
-                PrintInRed("No fictional variables found");
+                PrintInRed("Фиктивных переменных не найдено.");
             }
         }
         catch (Exception e)
         {
-            PrintInRed("It's not logical formula");
+            PrintInRed("Введённое выражение не является формулой сокращённого языка логики высказываний.");
         }
+    }
+
+    static string TranslateFormulaToInnerLanguage(string formula)
+    {
+        formula = formula.Replace("\\/", "|").Replace("/\\", "&");
+
+        for (int i = 0; i < formula.Length; i++)
+        {
+            if (char.IsLower(formula[i]) && char.IsLetter(formula[i]))
+            {
+                throw new Exception("Not logical formula");
+            }
+        }
+
+        return formula.ToLower();
     }
 
     static void PrintInRed(string content)
